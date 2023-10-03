d97c2a2eb0e74b586d48ea788395bfd5909918c9
DevExpressExampleBot <fakeEmail>
2023-10-03 16:23:39 +0400

    Source auto update [skip ci]

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,48 +1,43 @@
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Globalization
 Imports System.Windows.Forms
 Imports DevExpress.XtraSpellChecker
 
 Namespace SpellCheckerExample
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
 
-			LoadDictionaries()
-		End Sub
+    Public Partial Class Form1
+        Inherits Form
 
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
-			spellChecker1.Check(textBox1)
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+            LoadDictionaries()
+        End Sub
 
-		Private Sub LoadDictionaries()
-			Dim currentDirectory As String = System.IO.Directory.GetCurrentDirectory()
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            spellChecker1.Check(textBox1)
+        End Sub
 
-			Dim openOfficeDictionaryEnglish As New SpellCheckerOpenOfficeDictionary()
-			openOfficeDictionaryEnglish.DictionaryPath = currentDirectory & "\..\..\Dictionaries\en_US\en_US.dic"
-			openOfficeDictionaryEnglish.GrammarPath = currentDirectory & "\..\..\Dictionaries\en_US\en_US.aff"
-			openOfficeDictionaryEnglish.Culture = New CultureInfo("en-US")
-			spellChecker1.Dictionaries.Add(openOfficeDictionaryEnglish)
-
-			Dim openOfficeDictionarySpanish As New SpellCheckerOpenOfficeDictionary()
-			openOfficeDictionarySpanish.DictionaryPath = currentDirectory & "\..\..\Dictionaries\es_ES\es_ES.dic"
-			openOfficeDictionarySpanish.GrammarPath = currentDirectory & "\..\..\Dictionaries\es_ES\es_ES.aff"
-			openOfficeDictionarySpanish.Culture = New CultureInfo("es-ES")
-			spellChecker1.Dictionaries.Add(openOfficeDictionarySpanish)
-
-			Dim customDictionary As New SpellCheckerCustomDictionary()
-			customDictionary.AlphabetPath = currentDirectory & "\..\..\Dictionaries\EnglishAlphabet.txt"
-			customDictionary.DictionaryPath = currentDirectory & "\..\..\Dictionaries\CustomEnglish.dic"
-			customDictionary.Culture = CultureInfo.InvariantCulture
-			spellChecker1.Dictionaries.Add(customDictionary)
-
-			'spellChecker1.Culture = new CultureInfo("en-US");
-			'spellChecker1.Culture = new CultureInfo("es-ES");
-
-			spellChecker1.Culture = CultureInfo.InvariantCulture
-			spellChecker1.SpellCheckMode = SpellCheckMode.AsYouType
-		End Sub
-	End Class
+        Private Sub LoadDictionaries()
+            Dim currentDirectory As String = IO.Directory.GetCurrentDirectory()
+            Dim openOfficeDictionaryEnglish As SpellCheckerOpenOfficeDictionary = New SpellCheckerOpenOfficeDictionary()
+            openOfficeDictionaryEnglish.DictionaryPath = currentDirectory & "\..\..\Dictionaries\en_US\en_US.dic"
+            openOfficeDictionaryEnglish.GrammarPath = currentDirectory & "\..\..\Dictionaries\en_US\en_US.aff"
+            openOfficeDictionaryEnglish.Culture = New CultureInfo("en-US")
+            spellChecker1.Dictionaries.Add(openOfficeDictionaryEnglish)
+            Dim openOfficeDictionarySpanish As SpellCheckerOpenOfficeDictionary = New SpellCheckerOpenOfficeDictionary()
+            openOfficeDictionarySpanish.DictionaryPath = currentDirectory & "\..\..\Dictionaries\es_ES\es_ES.dic"
+            openOfficeDictionarySpanish.GrammarPath = currentDirectory & "\..\..\Dictionaries\es_ES\es_ES.aff"
+            openOfficeDictionarySpanish.Culture = New CultureInfo("es-ES")
+            spellChecker1.Dictionaries.Add(openOfficeDictionarySpanish)
+            Dim customDictionary As SpellCheckerCustomDictionary = New SpellCheckerCustomDictionary()
+            customDictionary.AlphabetPath = currentDirectory & "\..\..\Dictionaries\EnglishAlphabet.txt"
+            customDictionary.DictionaryPath = currentDirectory & "\..\..\Dictionaries\CustomEnglish.dic"
+            customDictionary.Culture = CultureInfo.InvariantCulture
+            spellChecker1.Dictionaries.Add(customDictionary)
+            'spellChecker1.Culture = new CultureInfo("en-US");
+            'spellChecker1.Culture = new CultureInfo("es-ES");
+            spellChecker1.Culture = CultureInfo.InvariantCulture
+            spellChecker1.SpellCheckMode = SpellCheckMode.AsYouType
+        End Sub
+    End Class
 End Namespace
